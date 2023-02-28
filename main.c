095cdfb8ad7ce4832a6fe7d6e267ce6febe5f35e
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-02-28 10:38:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 void gd_putchar(char c)
 {
     write(1, &c, 1);
@@ -9,10 +8,6 @@ void gd_putchar(char c)
 
 int gd_len_nombre(int nbr)
 {
-    if(nbr < 0)
-    {
-        nbr *= -1;
-    }
     int longueur = 1;
     while(nbr > 10)
     {
@@ -24,18 +19,17 @@ int gd_len_nombre(int nbr)
 
 void gd_putnbr(int nbr)
 {
-    int tmp = gd_len_nombre(nbr);
-    if(nbr < 0)
+    if(nbr<0)
     {
         gd_putchar('-');
         nbr *= -1;
     }
+    int tmp = gd_len_nombre(nbr);
     while(tmp > 0)
     {
-        gd_putchar(nbr / tmp + '0');
+        gd_putchar(nbr/tmp + '0');
         nbr %= tmp;
         tmp /= 10;
-        
     }
 }
 
@@ -49,13 +43,58 @@ int gd_strlen(char *str)
     return i;
 }
 
+int puissance_10(int nbr)
+{
+    int i = 1;
+    int res = 1;
+    while(i<nbr)
+    {
+        res *= 10;
+        i++;
+    }
+    return res;
+}
+int gd_atoi(char *str)
+{
+
+    int i = 0;
+    int nbr = 0;
+    int nega = 1;
+    while((str[i] == '-') || (str[i] >= '0' && str[i] <= '9'))
+    {
+        if(str[i] == '-')
+        {
+            nega = -1;
+        }
+        i++;
+    }
+    int j = 0;
+    if(nega == -1)
+    {
+        i--;
+        j = 1;
+    }
+    int len;
+    len = puissance_10(i);
+    while(str[j] >= '0' && str[j] <= '9')
+    {
+        nbr += (str[j] - '0') * len;
+        len /= 10;
+        j++;
+    }
+    return nbr * nega;
+}
+
 void gd_putstr(char *str)
 {
-    write(1 , str, gd_strlen(str));
+    write(1, str, gd_strlen(str));
 }
 
 int main()
 {
     gd_putnbr(-254);
+    gd_putchar('\n');
+    int gd = gd_atoi("-129299284ddcdv");
+    gd_putnbr(gd);
     gd_putchar('\n');
 }
